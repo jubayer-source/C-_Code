860d056ac6266ef06e171836b9558326732635a6
Md. Jubayer Ahmad <mdjubayerahmad16@gmail.com>
2024-06-29 22:21:06 +0600

    Add files via upload

---
+++ relational_operator.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    int a = 10, b = 20;
+
+    cout << "a == b: " << (a == b) << endl;
+
+    cout << "a == b: " << (a == b) << endl;
+    cout << "a != b: " << (a != b) << endl;
+    cout << "a > b: " << (a > b) << endl;
+    cout << "a < b: " << (a < b) << endl;
+    cout << "a >= b: " << (a >= b) << endl;
+    cout << "a <= b: " << (a <= b) << endl;
+
+
+    /*
+    Note : Comment for this bellow;
+
+    a == b is false (0) because 5 is not equal to 10.
+    a != b is true (1) because 5 is not equal to 10.
+    a > b is false (0) because 5 is not greater than 10.
+    a < b is true (1) because 5 is less than 10.
+    a >= b is false (0) because 5 is not greater than or equal to 10.
+    a <= b is true (1) because 5 is less than or equal to 10.
+    */
+
+
+
+
+
+
+
+
+    return 0;
+
+
+
+
+
+
+
+}
