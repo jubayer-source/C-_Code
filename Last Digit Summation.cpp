ede71a10c71f665caf316460c851b197777af09b
Md. Jubayer Ahmad <mdjubayerahmad16@gmail.com>
2024-07-05 00:47:47 +0600

    Create Last Digit Summation.cpp

---
+++ Last Digit Summation.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    long long M, N;
+    cin >> N>> M;
+
+    int digitN = N % 10, digitM = M % 10;
+
+    cout << digitN + digitM;
+
+
+
+    return 0;
+}
