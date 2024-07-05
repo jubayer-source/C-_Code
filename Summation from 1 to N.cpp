ad799ec71da36429900fcc02f9eace82cafb167d
Md. Jubayer Ahmad <mdjubayerahmad16@gmail.com>
2024-07-05 15:11:38 +0600

    Create Summation from 1 to N.cpp

---
+++ Summation from 1 to N.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    long long N;
+    cin >> N;
+
+    long long summaton = ( N * (N+1) ) / 2;
+
+    cout << summaton;
+
+    return 0;
+}
