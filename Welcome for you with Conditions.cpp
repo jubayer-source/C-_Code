f84f356a839979cecfe839f0dd1355ae25ff6924
Md. Jubayer Ahmad <mdjubayerahmad16@gmail.com>
2024-07-05 18:40:26 +0600

    Create  Welcome for you with Conditions.cpp

---
+++ Welcome for you with Conditions.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A  >> B;
+
+    if (A >= B)
+    {
+        cout << "Yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+
+
+    return 0;
+}
